f77a7bdd2be2e55cb75f92599b85a34045c773f6
YAMAGAMI Satoshi <yamagami_satoshi@e-grid.co.jp>
2023-04-11 20:20:58 +0900

    test:items create.

---
+++ tests/Http/Controllers/Admin/ItemCrudControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Http\Controllers\Admin;
+
+use App\Http\Controllers\Admin\ItemCrudController;
+use PHPUnit\Framework\TestCase;
+
+class ItemCrudControllerTest extends TestCase
+{
+
+    public function testCreate()
+    {
+
+    }
+}
